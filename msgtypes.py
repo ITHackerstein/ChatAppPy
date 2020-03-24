b58294d8240a2309c09f61705f4289b572334136
ITHackestein <carelladavide1@gmail.com>
2020-03-24 17:32:20 +0100

    This is a patch that adds new message types, changes the way of the data parsing and uses usernames to identify users connected

--- msgtypes.py
+++ msgtypes.py
@@ -1,5 +1,16 @@
 from enum import IntEnum
 
+"""
+Message structure:
+| type (1 byte) | [ args ]
+
+-- type = 0 (CloseConnection)
+-- type = 1 (OpenConnection), args = ['username: (1 byte 'size', 'size' bytes username)]
+-- type = 2 (UsernameSet), args = ['username: (1 byte 'size', 'size' bytes username)]
+-- type = 3 (SendMsg), args = ['msg (max 1024 bytes)']
+-- type = 4 (RecvMsg), args = ['username: (1 byte 'size', 'size' bytes username)', 'msg (max 1024 bytes)']
+
+"""
 
 class MsgTypes(IntEnum):
 	CloseConnection = 0
--- server.py
+++ server.py
@@ -3,58 +3,50 @@ import os
 import socket
 import threading
 from enum import IntEnum
+from msgtypes import MsgTypes
+from random import randint
 
-"""
-Message structure:
-| type (1 byte) | [ args ]
-
--- type = 0 (CloseConnection), args = ['addr']
--- type = 1 (SendMsg), args = ['msg (max 4096 bytes)']
-
-"""
-
-HOST, PORT = 'localhost', 5555
+HOST, PORT = "localhost", 5555
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connected_devices = []
-
-
-class MsgTypes(IntEnum):
-	CloseConnection = 0
-	SendMsg = 1
 
 class Device:
 	def __init__(self, conn):
 		self.conn = conn
+		self.username = ""
 		self.receive_data_t = threading.Thread(name="receive_data", target=self.receive_data)
 		self.receive_data_t.start()
 
 	def receive_data(self):
 		while self.conn.fileno() != -1:
-			data = self.conn.recv(4097)
-			if not data:
-				continue
+			type_ = ord(self.conn.recv(1))
 
-			i = 0
-			while i < len(data):
-				if data[i] == MsgTypes.CloseConnection:
-					print("[{}:{}] Connection closed!".format(*self.conn.getpeername()))
-					self.conn.close()
-					break
-				elif data[i] == MsgTypes.SendMsg:
-					i += 1
-					msg = data[i:]
-					i += len(msg)
-					for device in connected_devices:
-						if device.conn.getpeername() != self.conn.getpeername() and device.conn.fileno() != -1:
-							device.conn.sendall(msg)
-				else:
-					print("Garbage received from {}: {}".format(self.conn.getpeername(), data))
+			if type_ == MsgTypes.CloseConnection:
+				print("[{}:{}] Connection closed!".format(*self.conn.getpeername()))
+				self.conn.close()
+			elif type_ == MsgTypes.OpenConnection:
+				username_len = ord(self.conn.recv(1))
+				username = self.conn.recv(username_len).decode()
+
+				self.username = "%s#%04d" % (username, randint(0, 9999))
+				self.conn.sendall(chr(MsgTypes.UsernameSet).encode() + chr(len(self.username)).encode() + self.username.encode())
+
+				print("[{}:{}] Connected with username {}".format(*self.conn.getpeername(), self.username))
+			elif type_ == MsgTypes.SendMsg:
+				msg = self.conn.recv(1024)
+				if not self.username:
+					continue
+
+				for device in connected_devices:
+					device.conn.sendall(chr(MsgTypes.RecvMsg).encode() + chr(len(self.username)).encode() + self.username.encode() + msg)
+			else:
+				pass
+
 
 def get_connections():
 	while True:
 		conn, addr = sock.accept()
 		connected_devices.append(Device(conn))
-		print("[{}:{}] Connection opened!".format(*conn.getpeername()))
 
 sock.bind((HOST, PORT))
 sock.listen()
